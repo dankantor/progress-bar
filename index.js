40d9177e3ea6df36ac16a8a04befc09a6a62a120
Dan Kantor <dan.kantor@gmail.com>
2019-01-09 18:00:59 -0500

    moving to es6 class (wip)

--- index.js
+++ index.js
@@ -1,268 +1,153 @@
-(function(){
-
-"use strict";
-
-function ProgressBar(opts){
-
-    // class added to hide elements
+class ProgressBar {
+  
+  constructor(opts) {
     this.hideClass = 'display_none';
-    
-    // css class to add to progress bar when in loading state
-    this.loadingClass = "loading";
-
-    $.extend(this, opts);
-        
-    if(this.playQueue){
-        this.audio = this.playQueue.audio;
+    this.loadingClass = 'loading';
+    Object.assign(this, opts);
+    if (this.playQueue) {
+      this.audio = this.playQueue.audio;
     }
-    
-    // boolean if user is seeking or not
     this.isSeeking = false;
-    
     this.addListeners();
     this.setSizes();
-    
-}
-
-// add listeners
-ProgressBar.prototype.addListeners = function(){
+  }
+  
+  addListeners() {
     this.removeListeners();
-    if(this.back){
-        this.bindedBackClick = $.proxy(this, 'click');
-        $(this.back).bind(
-            'click', 
-            this.bindedBackClick
-        );
+    if (this.back) {
+      this.bindedBackClick = this.onClick.bind(this);
+      document.querySelector(this.back).addEventListener('click', this.bindedBackClick);
     }
-    if(this.front){
-        this.bindedFrontClick = $.proxy(this, 'click');
-        $(this.front).bind(
-            'click', 
-            this.bindedFrontClick
-        );
+    if (this.front) {
+      this.bindedFrontClick = this.onClick.bind(this);
+      document.querySelector(this.front).addEventListener('click', this.bindedFrontClick);
     }
-    if(this.thumb){
-        this.bindedThumbDown = $.proxy(this, 'mouseDown');
-        $(this.thumb).bind(
-            'mousedown', 
-            this.bindedThumbDown
-        );
-        this.bindedThumbUp = $.proxy(this, 'mouseUp');
-        $(this.thumb).bind(
-            'mouseup', 
-            this.bindedThumbUp
-        );
+    if (this.thumb) {
+      this.bindedThumbDown = this.onMouseDown.bind(this);
+      document.querySelector(this.thumb).addEventListener('mousedown', this.bindedThumbDown);
+      this.bindedThumbUp = this.onMouseUp.bind(this);
+      document.querySelector(this.thumb).addEventListener('mouseup', this.bindedThumbUp);
     }
-    if(this.playQueue){
-        this.bindedPlayQueueLoading = this.onLoading.bind(this);
-        this.playQueue.addEventListener(
-            'loading', 
-            this.bindedPlayQueueLoading, 
-            false
-        );
-        this.bindedPlayQueuePlaying = this.onPlaying.bind(this);
-        this.playQueue.addEventListener(
-            'playing', 
-            this.bindedPlayQueuePlaying, 
-            false
-        );
+    if (this.playQueue) {
+      this.bindedPlayQueueLoading = this.onLoading.bind(this);
+      this.playQueue.on('loading', this.bindedPlayQueueLoading);
+      this.bindedPlayQueuePlaying = this.onPlaying.bind(this);
+      this.playQueue.on('playing', this.bindedPlayQueuePlaying);
+    } else if (this.audio) {
+      this.bindedAudioLoading = this.onLoading.bind(this);
+      this.audio.addEventListener('loadstart', this.bindedAudioLoading);
+      this.bindedAudioPlaying = this.onPlaying.bind(this);
+      this.audio.addEventListener('canplay', this.bindedAudioPlaying);
     }
-    else{
-        if(this.audio){
-            this.bindedAudioLoading = this.onLoading.bind(this);
-            this.audio.addEventListener(
-                'loadstart', 
-                this.bindedAudioLoading, 
-                false
-            );
-            this.bindedAudioPlaying = this.onPlaying.bind(this);
-            this.audio.addEventListener(
-                'canplay', 
-                this.bindedAudioPlaying, 
-                false
-            );
-        }
+    if (this.audio) {
+      this.bindedTimeUpdate = this.onTimeUpdate.bind(this);
+      this.audio.addEventListener('timeupdate', this.bindedTimeUpdate);
+      this.bindedDurationChange = this.onDurationChange.bind(this);
+      this.audio.addEventListener('durationchange', this.bindedDurationChange);
+      this.bindedSeeking = this.onSeeking.bind(this);
+      this.audio.addEventListener('seeking', this.bindedSeeking);
+      this.bindedSeeked = this.onSeeked.bind(this);
+      this.audio.addEventListener('seeked', this.bindedSeeked);
+      this.bindedProgress = this.onProgress.bind(this);
+      if (this.loadingProgress) {
+        this.audio.addEventListener('progress', this.bindedProgress);
+      }
     }
-    if(this.audio){
-        this.bindedTimeUpdate = this.onTimeUpdate.bind(this);
-        this.audio.addEventListener(
-            'timeupdate', 
-            this.bindedTimeUpdate, 
-            false
-        );
-        this.bindedDurationChange = this.onDurationChange.bind(this);
-        this.audio.addEventListener(
-            'durationchange', 
-            this.bindedDurationChange, 
-            false
-        );
-        this.bindedSeeking = this.onSeeking.bind(this);
-        this.audio.addEventListener(
-            'seeking', 
-            this.bindedSeeking, 
-            false
-        );
-        this.bindedSeeked = this.onSeeked.bind(this);
-        this.audio.addEventListener(
-            'seeked', 
-            this.bindedSeeked, 
-            false
-        );
-        this.bindedProgress = this.onProgress.bind(this);
-        if(this.loadingProgress){
-            this.audio.addEventListener(
-                'progress', 
-                this.bindedProgress, 
-                false
-            );
-        }
+    this.bindedSetSizes = this.setSizes.bind(this);
+    window.addEventListener('resize', this.bindedSetSizes);
+  }
+  
+  removeListeners() {
+    document.querySelector(this.back).removeEventListener('click', this.bindedBackClick);
+    document.querySelector(this.front).removeEventListener('click', this.bindedFrontClick);
+    document.querySelector(this.thumb).removeEventListener('mousedown', this.bindedThumbDown);
+    document.querySelector(this.thumb).removeEventListener('mouseup', this.bindedThumbUp);
+    if (this.playQueue) {
+      this.playQueue.off('loading', this.bindedPlayQueueLoading);
+      this.playQueue.off('playing', this.bindedPlayQueuePlaying);
     }
-    this.bindedSetSizes = $.proxy(this, 'setSizes');
-    $(window).bind(
-        'resize', 
-        this.bindedSetSizes
-    );
-}
-
-
-// remove all listeners
-ProgressBar.prototype.removeListeners = function(){
-    $(this.back).unbind(
-        'click', 
-        this.bindedBackClick
-    );
-    $(this.front).unbind(
-        'click', 
-        this.bindedFrontClick
-    );
-    $(this.thumb).unbind(
-        'mousedown', 
-        this.bindedThumbDown
-    );
-    $(this.thumb).unbind(
-        'mouseup', 
-        this.bindedThumbUp
-    );
-    if(this.playQueue){
-        this.playQueue.removeEventListener(
-            'loading', 
-            this.bindedPlayQueueLoading
-        );
-        this.playQueue.removeEventListener(
-            'playing', 
-            this.bindedPlayQueuePlaying
-        );
+    this.audio.removeEventListener('loadstart', this.bindedAudioLoading);
+    this.audio.removeEventListener('canplay', this.bindedAudioPlaying);
+    this.audio.removeEventListener('timeupdate', this.bindedTimeUpdate);
+    this.audio.removeEventListener('durationchange', this.bindedDurationChange);
+    this.audio.removeEventListener('seeking', this.bindedSeeking);
+    this.audio.removeEventListener('seeked', this.bindedSeeked);
+    this.audio.removeEventListener('progress', this.bindedProgress);
+    window.removeEventListener('resize', this.bindedSetSizes);
+  }
+  
+  // todo get with w/o jquery
+  setSizes(){
+    if (this.back) {
+      this.width = $(this.back).width();
+      this.left = $(this.back).offset().left;
+      this.right = this.left + this.width;
     }
-    this.audio.removeEventListener(
-        'loadstart', 
-        this.bindedAudioLoading 
-    );
-    this.audio.removeEventListener(
-        'canplay', 
-        this.bindedAudioPlaying
-    );
-    this.audio.removeEventListener(
-        'timeupdate', 
-        this.bindedTimeUpdate
-    );
-    this.audio.removeEventListener(
-        'durationchange', 
-        this.bindedDurationChange
-    );
-    this.audio.removeEventListener(
-        'seeking', 
-        this.bindedSeeking
-    );
-    this.audio.removeEventListener(
-        'seeked', 
-        this.bindedSeeked 
-    );
-    this.audio.removeEventListener(
-        'progress', 
-        this.bindedProgress 
-    );
-    $(window).unbind(
-        'resize', 
-        this.bindedSetSizes
-    );
-}
-
-// calculate widths
-ProgressBar.prototype.setSizes = function(){
-    if(this.back){
-        this.width = $(this.back).width();
-        this.left = $(this.back).offset().left;
-        this.right = this.left + this.width;
-    }
-}
-
-// reset everything to default 
-ProgressBar.prototype.reset = function(){
-    $(this.thumb).addClass(this.hideClass);
-    $(this.front).addClass(this.hideClass);
-    this.currentTimeText = "0:00";
-    this.durationText = "0:00"; 
-    this.timeBreakText = "";
+  }
+  
+  reset() {
+    document.querySelector(this.thumb).classList.add(this.hideClass);
+    document.querySelector(this.front).classList.add(this.hideClass);
+    this.currentTimeText = '0:00';
+    this.durationText = '0:00'; 
+    this.timeBreakText = '';
     this.thumbLeft = 0;
     this.frontWidth = -101;
     this.percentageWidth = -100;
     this.requestAnimationFrame(this.draw);
-}
-
-// onLoading event, reset times, reset thumb and front, add loading class to back
-ProgressBar.prototype.onLoading = function(e){
+  }
+  
+  onLoading(e) {
     this.reset()
-    $(this.back).addClass(this.loadingClass);
+    document.querySelector(this.back).classList.add(this.loadingClass);
     this.timeBreakText = "/";
-}
-
-// onPlaying event. show thumb and front, remove loading class from back 
-ProgressBar.prototype.onPlaying = function(e){
-    $(this.thumb).removeClass(this.hideClass);
-    $(this.front).removeClass(this.hideClass);
-    $(this.back).removeClass(this.loadingClass);
-}
-
-// onTimeUpdate event. Update count and duration. Set width on front. Move thumb.
-ProgressBar.prototype.onTimeUpdate = function(e){
+  }
+  
+  onPlaying(e) {
+    document.querySelector(this.thumb).classList.remove(this.hideClass);
+    document.querySelector(this.front).classList.remove(this.hideClass);
+    document.querySelector(this.back).classList.remove(this.loadingClass);
+  }
+  
+  onTimeUpdate(e) {
     this.setPosition(e.target);
-}
-
-// onDurationChange event. Update duration. 
-ProgressBar.prototype.onDurationChange = function(e){
+  }
+  
+  onDurationChange(e) {
     this.durationText = this.getMMSS(Math.floor(e.target.duration)); 
     this.requestAnimationFrame(this.draw);
-}
-
-// onSeeking event, add loading class to back
-ProgressBar.prototype.onSeeking = function(e){
-    $(this.back).addClass(this.loadingClass);
-}
-
-// onSeeked event, remove loading class to back
-ProgressBar.prototype.onSeeked = function(e){
-    $(this.back).removeClass(this.loadingClass);
-}
-
-// onProgress event, fired when media is loading
-ProgressBar.prototype.onProgress = function(e){
+  }
+  
+  onSeeking(e) {
+    document.querySelector(this.back).classList.add(this.loadingClass);
+  }
+  
+  onSeeked(e) {
+    document.querySelector(this.back).classList.remove(this.loadingClass);
+  }
+  
+  onProgress(e) {
     this.percentageWidth = (100 * (e.target.buffered.end / e.target.duration)) - 100;
     this.requestAnimationFrame(this.draw);
-}
-
-// mouseDown on thumb listener
-ProgressBar.prototype.mouseDown = function(e){
-    $(document).bind(
-        'mousemove', 
-        $.proxy(this, 'mouseMove')
-    );
-    $(document).bind(
-        'mouseup', 
-        $.proxy(this, 'mouseUp')
-    );
+  }
+  
+  onMouseDown(e) {
+    document.addEventListener('mousemove', this.onMouseMove.bind(this);
+    document.addEventListener('mouseup', this.onMouseUp.bind(this);
     this.isSeeking = true;
     e.preventDefault();
+  }
+  
+  
 }
+
+export {ProgressBar};
+
+
+
+
+
+
 
 // mouseMove on thumb listener
 ProgressBar.prototype.mouseMove = function(e){
